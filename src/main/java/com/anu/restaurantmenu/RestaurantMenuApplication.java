7267da8678c213417d7f9c9bac690bfa316039d5
anuradha <anuradha.kadurugasyaya@gmail.com>
2024-02-25 10:07:30 +0800

    first commit

---
+++ src/main/java/com/anu/restaurantmenu/RestaurantMenuApplication.java
@@ -0,0 +1,13 @@
+package com.anu.restaurantmenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantMenuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anu/restaurantmenu/RestuarantMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anu.restaurantmenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestuarantMenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
